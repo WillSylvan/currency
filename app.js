2a9da5b3a97deb7c7e770cbefabb2b156ae10510
WillSylvan <silvis.silvestrs@gmail.com>
2016-11-08 20:15:55 +0200

    exchangeRateLVLTOUSD

--- app.js
+++ app.js
@@ -11,8 +11,22 @@ let usdToLvl = usd => {
     let eur = usd / exchangeRates.usd  
     //  2.eur => lvl 
      let lvl = eur * exchangeRates.lvl
+
+     return lvl
 }
 
+
+let lvlToUsd = lvl => {
+    // 1. lvl => usd
+    let eur = lvl * exchangeRates.lvl
+    // 2. usd => eur
+    let usd = eur / exchangeRates.usd
+
+    return usd
+}
+
+
 let lvl = usdToLvl(10) 
+let usd = lvlToUsd(1)
 
 
